2c5febc3eeddbac14db528be8fc98006ba2fccd1
Gabriel <engs-gabrielgoes@camporeal.edu.br>
2026-05-27 19:23:12 -0300

    adicionado excecoes de validação e estoque

---
+++ exception/PedidoNotFoundException.java
@@ -0,0 +1,12 @@
+package exception;
+ 
+public class PedidoNotFoundException extends RuntimeException {
+ 
+    public PedidoNotFoundException(String mensagem) {
+        super(mensagem);
+    }
+ 
+    public PedidoNotFoundException(int pedidoId) {
+        super("Pedido não encontrado com o ID: " + pedidoId);
+    }
+}
---
+++ exception/ValidacaoException.java
@@ -0,0 +1,9 @@
+package exception;
+ 
+public class ValidacaoException extends RuntimeException {
+ 
+    public ValidacaoException(String mensagem) {
+        super(mensagem);
+    }
+}
+ 
